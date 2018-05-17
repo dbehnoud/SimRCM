fe1f94cf6e1db98cce3b347a98fdc3d106849861
dbehnoud <38480024+dbehnoud@users.noreply.github.com>
2018-05-16 21:06:02 -0700

    Update test_sim.py

--- simRCM/tests/test_sim.py
+++ simRCM/tests/test_sim.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_sim():
 
- gas = sim.simulation()
+ gas = reaction_chamber.simulation()
  
  assert gas.T == pytest.approx(2869.88, rel=1e-1)
  assert gas.P == pytest.approx(259246, rel=1e-1)
