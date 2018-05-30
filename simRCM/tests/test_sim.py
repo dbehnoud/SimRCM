de3225bd08060bc1841a6472d4a0f37d05760086
diba <d.behnoudfar@gmail.com>
2018-05-30 10:32:58 -0700

    added docstrings

--- simRCM/tests/test_sim.py
+++ simRCM/tests/test_sim.py
@@ -2,8 +2,12 @@ from .. import reaction_chamber
 import pytest
 
 def test_sim():
+    
+    """
+    check return for two datapoints
+    """
 
- gas = reaction_chamber.simulation()
- 
- assert gas.T == pytest.approx(2869.88, rel=1e-1)
- assert gas.P == pytest.approx(259246, rel=1e-1)
+    gas = reaction_chamber.simulation()
+     
+    assert gas.T == pytest.approx(2869.88, rel=1e-1)
+    assert gas.P == pytest.approx(259246, rel=1e-1)
