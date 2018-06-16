d57bce0cdc04530f2d28fffab07bc85419bbde0d
dbehnoud <38480024+dbehnoud@users.noreply.github.com>
2018-06-15 21:27:00 -0400

    Update test_zone.py

--- simrcm/tests/test_zone.py
+++ simrcm/tests/test_zone.py
@@ -13,21 +13,21 @@ def test_zone():
     
     zone = simrcm.sim_tools.def_zones(z, bore, t0, v_rcm, a_rcm)
     
-    assert zone[1].radius == 9.27566302
-    assert zone[1].height == 14.55132604
-    assert zone[1].thickness == 9.27566302
-    assert zone[1].volume == 3933.16684353
-    assert zone[1].surface_area == 1388.65382985
+    assert zone[1].radius[0] == 9.27566302
+    assert zone[1].height[0] == 14.55132604
+    assert zone[1].thickness[0] == 9.27566302
+    assert zone[1].volume[0] == 3933.16684353
+    assert zone[1].surface_area[0] == 1388.65382985
     
-    assert zone[3].radius == 17.2831974
-    assert zone[3].height == 30.56639479
-    assert zone[3].thickness == 2.94741118
-    assert zone[3].volume == 12755.12408476
-    assert zone[3].surface_area == 5196.15621734
+    assert zone[3].radius[0] == 17.2831974
+    assert zone[3].height[0] == 30.56639479
+    assert zone[3].thickness[0] == 2.94741118
+    assert zone[3].volume[0] == 12755.12408476
+    assert zone[3].surface_area[0] == 5196.15621734
     
     assert zone[5].radius == 20.0
     assert zone[5].height == 36.0
-    assert zone[5].thickness == 1.0
-    assert zone[5].volume == 6679.02598153
+    assert zone[5].thickness[0] == 1.0
+    assert zone[5].volume[0] == 6679.02598153
     assert zone[5].surface_area == 7037.167544041136
     
