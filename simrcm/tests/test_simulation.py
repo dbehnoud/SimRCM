00224eb4c93b64949143418f95b8fa7ee439f575
dbehnoud <38480024+dbehnoud@users.noreply.github.com>
2018-06-15 20:07:27 -0400

    Update test_simulation.py

--- simrcm/tests/test_simulation.py
+++ simrcm/tests/test_simulation.py
@@ -5,9 +5,5 @@ import pytest
 
 def test_1():
     ig1, p1, temp1, t1 = simrcm.simulation.simulation1('simrcm/tests/example_input.yaml')
-    assert ig1 == pytest.approx(0.003, rel=1e-1)
-
-   
-    
-    
+    assert ig1 == pytest.approx(0.03, rel=1e-1)
     
