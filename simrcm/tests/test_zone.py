8371b49cd61e622150775b49375de7ae1050534c
dbehnoud <38480024+dbehnoud@users.noreply.github.com>
2018-06-15 21:36:25 -0400

    Update test_zone.py

--- simrcm/tests/test_zone.py
+++ simrcm/tests/test_zone.py
@@ -13,21 +13,21 @@ def test_zone():
     
     zone = simrcm.sim_tools.def_zones(z, bore, t0, v_rcm, a_rcm)
     
-    assert zone[1].radius == 9.27566302
-    assert zone[1].height == 14.55132604
-    assert zone[1].thickness == 9.27566302
-    assert zone[1].volume == 3933.16684353
-    assert zone[1].surface_area == 1388.65382985
+    assert zone[1].radius == pytest.approx(9.27566302, rel=1e-1)
+    assert zone[1].height == pytest.approx(14.55132604, rel=1e-1)
+    assert zone[1].thickness == pytest.approx(9.27566302, rel=1e-1)
+    assert zone[1].volume == pytest.approx(3933.16684353, rel=1e-1)
+    assert zone[1].surface_area == pytest.approx(1388.65382985, rel=1e-1)
     
-    assert zone[3].radius == 17.2831974
-    assert zone[3].height == 30.56639479
-    assert zone[3].thickness == 2.94741118
-    assert zone[3].volume == 12755.12408476
-    assert zone[3].surface_area == 5196.15621734
+    assert zone[3].radius == pytest.approx(17.2831974, rel=1e-1)
+    assert zone[3].height == pytest.approx(30.56639479, rel=1e-1)
+    assert zone[3].thickness == pytest.approx(2.94741118, rel=1e-1)
+    assert zone[3].volume == pytest.approx(12755.12408476, rel=1e-1)
+    assert zone[3].surface_area == pytest.approx(5196.15621734, rel=1e-1)
     
-    assert zone[5].radius == 20.0
-    assert zone[5].height == 36.0
-    assert zone[5].thickness == 1.0
-    assert zone[5].volume == 6679.02598153
-    assert zone[5].surface_area == 7037.167544041136
+    assert zone[5].radius == pytest.approx(20.0, rel=1e-1)
+    assert zone[5].height == pytest.approx(36.0, rel=1e-1)
+    assert zone[5].thickness == pytest.approx(1.0, rel=1e-1)
+    assert zone[5].volume == pytest.approx(6679.02598153, rel=1e-1)
+    assert zone[5].surface_area == pytest.approx(7037.167544041136, rel=1e-1)
     
