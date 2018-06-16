c43c77f1bea4924aede92e74add5a6b3a4acf44a
dbehnoud <38480024+dbehnoud@users.noreply.github.com>
2018-06-15 20:16:00 -0400

    Update and rename zone_test.py to test_zone.py

---
+++ simrcm/tests/test_zone.py
@@ -0,0 +1,40 @@
+import numpy as np
+from numpy.polynomial.polynomial import polyval
+    
+def def_zones(t0,z,bore,alpha,a_rcm,v_total):        
+    zone = [0]
+    for x in range(1,z+1):
+        zone.append(Zone(x))
+    return zone
+
+
+def test_zone():
+    t0 = 1
+    z =5
+    bore = 40
+    alpha = 2
+    a_rcm = np.pi/4*bore**2
+    v_total = a_rcm*36
+    
+    zone = def_zones(t0,z,bore,alpha,a_rcm,v_total)
+    
+    assert zone[1].radius == 5
+    assert zone[1].height == 6
+    assert zone[1].thickness == 5
+    assert zone[1].volume == np.pi*25*6
+    assert zone[1].surface_area == 2*np.pi*25 + 2*np.pi*30
+    assert zone[1].cross_area == np.pi*25
+    
+    assert zone[3].radius == 17
+    assert zone[3].height == 30
+    assert zone[3].thickness == 4
+    assert zone[3].volume == np.pi*17**2*30 - np.pi*13**2*22
+    assert zone[3].surface_area == 2*np.pi*17**2 + 2*np.pi*17*30
+    assert zone[3].cross_area == np.pi*17**2
+    
+    assert zone[5].radius == 20
+    assert zone[5].height == 36
+    assert zone[5].thickness == 1
+    assert zone[5].volume == np.pi*400*36 - np.pi*19**2*34
+    assert zone[5].surface_area == 2*np.pi*400 + 2*np.pi*20*36
+    assert zone[5].cross_area == np.pi*400
