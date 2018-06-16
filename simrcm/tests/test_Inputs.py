f83397bb687c23e3c1e07ac540a40b48cffe89ef
dbehnoud <38480024+dbehnoud@users.noreply.github.com>
2018-06-15 21:29:55 -0400

    Update test_Inputs.py

--- simrcm/tests/test_Inputs.py
+++ simrcm/tests/test_Inputs.py
@@ -3,7 +3,7 @@ import simrcm.sim_tools
 
 #assert inputs.z == 10
 def test_bore():
-       inputs = simrcm.sim_tools.Inputs('simrcm/example_input.yaml')
+       inputs = simrcm.sim_tools.Inputs('simrcm/tests/example_input.yaml')
        assert inputs.bore == 0.0508 
 #assert inputs.v_rcm == 5.47669375000E+002         
 #assert inputs.vprofile['vproVol'][2] == 5.43427034574E+002     
